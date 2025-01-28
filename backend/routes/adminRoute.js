112d6d83542a0f1977241f96382623a1d9685bad
Prajjwal888 <chouhanprajjwal8@gmail.com>
2025-01-29 01:27:48 +0530

    integrate addStudent and viewStudent

--- backend/routes/adminRoute.js
+++ backend/routes/adminRoute.js
@@ -1,18 +1,28 @@
-import express from 'express';
-import {addAdmin, addFaculty, addStudent, deleteFaculty, deleteStudent, getAdmin, getAllAdmins, getFaculty, getStudent, loginHandler} from '../controllers/admin.js';
-import { adminMiddleware } from '../middleware/adminMiddleware.js';
+import express from "express";
+import {
+  addAdmin,
+  addFaculty,
+  addStudent,
+  deleteFaculty,
+  deleteStudent,
+  getAdmin,
+  getAllAdmins,
+  getFaculty,
+  getStudent,
+  loginHandler,
+} from "../controllers/admin.js";
+import { adminMiddleware } from "../middleware/adminMiddleware.js";
 const adminroute = express.Router();
 
-adminroute.post('/login', loginHandler);
-adminroute.post('/addAdmin',adminMiddleware,addAdmin);
-adminroute.get('/getAdmin',adminMiddleware,getAdmin);
-adminroute.get('/getallAdmin',adminMiddleware,getAllAdmins);
-adminroute.post("/addStudent",adminMiddleware,addStudent);
-adminroute.get("/getStudent",adminMiddleware,getStudent);
-adminroute.post("/addFaculty",adminMiddleware,addFaculty)
-adminroute.get("/getFaculty",adminMiddleware,getFaculty);
-adminroute.delete("/deleteFaculty",adminMiddleware,deleteFaculty);
-adminroute.delete("/deleteStudent",adminMiddleware,deleteStudent);
+adminroute.post("/login", loginHandler);
+adminroute.post("/addAdmin", adminMiddleware, addAdmin);
+adminroute.get("/getAdmin", adminMiddleware, getAdmin);
+adminroute.get("/getallAdmin", adminMiddleware, getAllAdmins);
+adminroute.post("/addStudent", adminMiddleware, addStudent);
+adminroute.post("/getStudent", adminMiddleware, getStudent);
+adminroute.post("/addFaculty", adminMiddleware, addFaculty);
+adminroute.get("/getFaculty", adminMiddleware, getFaculty);
+adminroute.delete("/deleteFaculty", adminMiddleware, deleteFaculty);
+adminroute.delete("/deleteStudent", adminMiddleware, deleteStudent);
 
 export default adminroute;
-
