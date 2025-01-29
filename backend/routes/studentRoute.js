5b76945b2d9c324e50c4b9a36cf0b659a714c935
arsalan <arsalanmoid1@gmail.com>
2025-01-29 21:04:10 +0530

    add branch and subject model

--- backend/routes/studentRoute.js
+++ backend/routes/studentRoute.js
@@ -3,12 +3,13 @@ import {loginHandler} from '../controllers/student.js';
 
 import { studentAuthMiddleware } from '../middleware/studentMiddleware.js';
 import { getTimeTable } from '../controllers/student.js';   
+import { getNotice } from '../controllers/notice.js';
 const studentRouter = express.Router();
 
 studentRouter.post('/login', loginHandler);
 
 
-studentRouter.get('/getNotice',studentAuthMiddleware,getNotice);
+studentRouter.get('/getNotice',studentAuthMiddleware,getNotice)
 studentRouter.get('/getTimeTable',studentAuthMiddleware,getTimeTable);
 
 export default studentRouter ;
