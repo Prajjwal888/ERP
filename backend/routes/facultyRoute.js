6ca7e8179bf35c8cd7ace1bc7ffe7b6aef31fcf2
Prajjwal888 <131273372+Prajjwal888@users.noreply.github.com>
2025-02-04 03:09:46 +0530

    Update facultyRoute.js

--- backend/routes/facultyRoute.js
+++ backend/routes/facultyRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import {loginHandler,getFaculty,getStudent, renderStudent, addMarks} from '../controllers/faculty.js';
+import {loginHandler,getFaculty,getStudent, renderStudent, addMarks, getFacultySubjects} from '../controllers/faculty.js';
 import facultyAuthMiddleware from '../middleware/facultyMiddleware.js';
 import upload from '../middleware/multerMiddleware.js';
 import { puttimeTable } from '../controllers/faculty.js';
@@ -13,7 +13,7 @@ facultyRouter.post('/login', loginHandler);
 facultyRouter.get('/getFaculty',facultyAuthMiddleware,getFaculty);
 facultyRouter.post('/getStudent',facultyAuthMiddleware,getStudent);
 facultyRouter.get('/getNotice',facultyAuthMiddleware,getNotice);
-facultyRouter.post('/uploadTimetable',facultyAuthMiddleware,upload.single('image'),puttimeTable);
+facultyRouter.post('/uploadTimetable',facultyAuthMiddleware,upload.single('file'),puttimeTable);
 facultyRouter.post('/uploadMaterial',facultyAuthMiddleware,upload.single('file'),putMaterial);
 facultyRouter.post("/renderStudent",facultyAuthMiddleware,renderStudent);
 facultyRouter.post("/addMarks",facultyAuthMiddleware,addMarks);
@@ -21,5 +21,6 @@ facultyRouter.get('/getBranch',facultyAuthMiddleware,getBranch);
 facultyRouter.post('/getSubject',facultyAuthMiddleware,getSubject);
 facultyRouter.post('/addNotice',facultyAuthMiddleware,addNotice);
 facultyRouter.delete('/deleteNotice/:id',facultyAuthMiddleware,deleteNotice);
+facultyRouter.get('/getFacultySubjects',facultyAuthMiddleware,getFacultySubjects);
 export default facultyRouter;
 
