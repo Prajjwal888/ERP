6604fbe98a7e15754ad11a6e262cda0a9c7faa9c
Prajjwal888 <131273372+Prajjwal888@users.noreply.github.com>
2025-02-08 19:08:41 +0530

    Update MaterialModel.js

--- backend/models/MaterialModel.js
+++ backend/models/MaterialModel.js
@@ -1,25 +1,21 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const materialSchema =new mongoose.Schema({
-   title:{
-        type:String,
-        required:true
-    },
-    subject:{
-        type: mongoose.Schema.Types.ObjectId, // Use ObjectId here
-        ref: "subject",
-        required: true
+const materialSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: mongoose.Schema.Types.ObjectId, // Use ObjectId here
+    ref: "subject",
+    required: true,
+  },
+  file: {
+    url: String,
+    filename: String,
+  },
+});
 
-    },
-    file:
-      {
-        type:String,
-        required:false,
-      }
-
-
-})
-
-const material =mongoose.model('material',materialSchema);
+const material = mongoose.model("material", materialSchema);
 
 export default material;
